737eeeb71d16d5471736ffd25b120b28fd4be5f2
meleelin <sdy2615380450@gmail.com>
2023-07-08 12:56:06 +0800

    fix: remove size.html

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,5 +5,9 @@
     <router-link to="/">Home</router-link>
     <router-link to="/login">Login</router-link>
   </div>
+  <p>
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure dicta explicabo ullam, quis hic ipsum impedit aliquid laboriosam veniam quo
+    voluptatem commodi ab eius. Esse itaque blanditiis iure nobis quod?
+  </p>
 </template>
 <style scoped></style>
